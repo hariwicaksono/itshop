8fa02e739188e3dfb2c23718c54cbe35c881494a
Hari Wicaksono <hariwicaksono87@gmail.com>
2021-03-30 22:33:19 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 //const isDev = process.env.NODE_ENV === 'development'
 const isProd = process.env.NODE_ENV === 'production'
-const basePath = '/';
+const basePath = '';
 
 module.exports = {
     basePath: isProd ? basePath : '',
